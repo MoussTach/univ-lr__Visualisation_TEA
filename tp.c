164eb9d06454d8e6df7ecb6d1acb46d5b756dfcf
Brenckle Gaetan <gaetan.brenckle@outlook.fr>
2021-04-27 21:29:45 +0200

    échantillon

--- tp.c
+++ tp.c
@@ -31,22 +31,28 @@
 
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #include "maillage.h"
 #include "calcul.h"
 
+typedef struct  infos {
+    int         nx;
+    int         ny;
+    double      angle;
+}               infos;
 
 void    charger_objet(char *nom) {
     FILE        *fobject = NULL;
-    int         **tabValues;
-    long long   **tabValues;
+    infos       infos;
+    float       **tabValues;
     int         nbLines;
+    int         posElem;
 
     char    *line = NULL;
-    char    *lineptr = NULL;
-    int 	index = 0;
     size_t	len = 0;
     ssize_t	read;
+    char    **splitLine = NULL;
 
     if(!(fobject = fopen(nom, "r"))) {
         fprintf(stderr, "Erreur lors de l'ouverture du fichier-objet\n");
@@ -55,10 +61,48 @@ void    charger_objet(char *nom) {
 
     nbLines = 0;
     while ((read = getline(&line, &len, fobject)) != -1) {
-        if (nbLines == 0) {
+        line = strtok(line, "\t\r\n ");
+        char *splitLine[5] = {NULL};
 
+        posElem = 0;
+        while (line) {
+            splitLine[posElem++] = line;
+
+            if (posElem > 4) {
+                fprintf(stderr, "Erreur, le fichier comporte plus d'argumets que demandé.\n");
+                exit(EXIT_FAILURE);
+            }
+            line = strtok(NULL, "\t\r\n ");
         }
-        printf("%s\n", line);
+
+        if (nbLines == 0) {
+            infos.nx = strtol(splitLine[0], NULL, 10);
+            infos.ny = strtol(splitLine[1], NULL, 10);
+            infos.angle = strtod(splitLine[2], NULL);
+
+            if (!(tabValues = malloc(sizeof(float *) * ((infos.nx * infos.ny)) + 1))) {
+                fprintf(stderr, "Erreur, malloc échoué.\n");
+                exit(EXIT_FAILURE);
+            }
+
+            posElem = 0;
+            while (posElem < ((infos.nx * infos.ny)) + 1) {
+                if (!(tabValues[posElem++] = malloc(sizeof(float) * 5))) {
+                    fprintf(stderr, "Erreur, malloc échoué.\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+        } else {
+            posElem = 0;
+            while (splitLine[posElem]) {
+                tabValues[nbLines - 1][posElem] = atof(splitLine[posElem]);
+                printf("%f\n", tabValues[nbLines - 1][posElem]);
+                posElem++;
+            }
+        }
+
+        printf("\n");
         nbLines += 1;
     }
 
